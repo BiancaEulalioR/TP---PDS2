66054f1ee1e66d1fb0a9eaecfae253a26f38f51b
Samaraquinof <samarasantosferreiradeaquino@gmail.com>
2026-04-25 11:26:10 -0300

    Especificação da classe Post

---
+++ Trabalho/include/Post.h
@@ -0,0 +1,32 @@
+#ifndef POST_H_
+#define POST_H_
+
+#include <string>
+#include <vector>
+
+
+class Post{
+    private:
+
+    //curtidas
+    int likes;
+
+    //comentários
+    std::vector <std::string> comments;
+
+    public:
+    //Inserir a curtida no post
+    void inserirLike();
+
+     //Remover a curtida post
+    void removerLike();
+
+     //Inserir um comentário no post
+    void inserirComment(std::string comment);
+
+    //Remover o comentário do post
+    void removerComment(int *indice);
+
+    };
+    
+    #endif
