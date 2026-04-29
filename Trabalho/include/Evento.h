fe133b54a6c9dd52fce5940ca9c6dd38ceda9229
Nick <portesvalina@gmail.com>
2026-04-28 21:24:14 -0300

    Organização do Evento.h

--- Trabalho/include/Evento.h
+++ Trabalho/include/Evento.h
@@ -7,12 +7,10 @@
 
 class Evento {
         private:
+
             std::string textoEvento; //Evento a ser inserido
-
             std::string contato; //Informações de contato
-
             Perfil organizador; //Perfil do organizador do evento
-
             Post post; //Para incluir curtidas e comentários
 
         public:
@@ -20,18 +18,13 @@ class Evento {
 
             //Getters
             std::string getTextoEvento();
-
             std::string getContato();
-            
             Perfil getOrganizador();
 
             //Metodos
             Evento criarEvento(std::string textoEvento, std::string contato, Perfil organizador);
-
-            void editarEvento(std::string textoEvento);
-
             Evento infoContato(std::string contato);
-
+            void editarEvento(std::string textoEvento);
             void excluirEvento(std::string textoEvento);
 
 };
