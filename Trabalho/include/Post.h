ed928614cf7150199b8766e692ecb421e205a0b5
Samaraquinof <samarasantosferreiradeaquino@gmail.com>
2026-05-17 18:36:13 -0300

    Refactor Post class to use map for comments
    Updated Post class to use a map for comments and added an ID for each comment.

--- Trabalho/include/Post.h
+++ Trabalho/include/Post.h
@@ -2,37 +2,31 @@
 #define POST_H_
 
 #include <string>
-#include <vector>
+#include <map>
 
 
 class Post{
     private:
 
-    //Guarda a quantidade de likes
     int likes;
-
-    //Guarda os comentários em um vector de string
-    std::vector <std::string> comments;
+    int idCont; //chave do map
+    std::map <int,std::string> comments;
 
     public:
-    //Inicializa Post com 0 likes 
+
     Post(); 
     
     void inserirLike();
 
     void removerLike();
 
-    //Insere um comentário no fim do post
     void inserirComment(std::string comment);
 
-    //Remove o comentário feito pelo perfil
-    void removerComment(int *indice);
+    void removerComment(int id);
 
-    //Retorna o número de likes do post;
     int getLikes();
 
-    //Retorna o número de comentários do post;
-    int getComment();
+    const std::map<int, std::string>& listarComments(); // passagem por referência
 
     };
     
