9cdf62c386da64f384b9334486d76ebaaab3e21b
Samaraquinof <samarasantosferreiradeaquino@gmail.com>
2026-05-17 18:36:25 -0300

    Implement Post class

--- Trabalho/scr/Post.cpp
+++ Trabalho/scr/Post.cpp
@@ -1,1 +1,33 @@
+#include "Post.h"
+
+
+    Post::Post(){
+      likes = 0;
+      idCont = 0; //chave do map comments
+    }
+    
+    void Post::inserirLike(){
+      likes += 1 ;
+    }
+
+    void Post::removerLike(){
+      likes -= 1 ;
+    }
+
+    void Post::inserirComment(std::string comment){
+      comments[idCont] = comment;
+      idCont++; //aumenta o índice do próximo comentário
+    }
+
+    void Post::removerComment(int id){
+      comments.erase(id);
+    }
+
+    int Post::getLikes(){
+      return likes;
+    }
+
+    const std::map<int, std::string>& Post::listarComments() {
+    return comments; 
+    }
 
