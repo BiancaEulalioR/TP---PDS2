d9cd12b881f9e9cfb29a9e521ab78cd54ff76be3
Samaraquinof <samarasantosferreiradeaquino@gmail.com>
2026-05-29 00:00:19 -0300

    Implementação da classe Spotted
    Implement Spotted class methods for creating, deleting, and listing posts.

--- Trabalho/scr/Spotted.cpp
+++ Trabalho/scr/Spotted.cpp
@@ -1,1 +1,21 @@
+#include "Post.h"
+#include "Spotted.h"
 
+
+    Spotted::Spotted() : Post() {
+        idCont = 0;
+     }
+
+    void Spotted::criarPost(std::string s){
+    lista[idCont] = s;
+    idCont++;
+    }
+
+    void Spotted::apagarPost(int indice){
+    lista.erase(indice);
+    //adicionar verificação de índice
+    }
+    
+    const std::map<int, std::string>& Spotted::listarPosts(){
+    return lista; //cout no main
+    }
