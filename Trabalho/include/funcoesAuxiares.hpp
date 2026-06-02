2355ec06deff65bf3678e8e0e2514bf94f41a2b5
BibsHubbie <biancaeulalioramos@gmail.com>
2026-06-02 16:35:12 -0300

    continuando switch de servico e fazendo funcao auxiliar com template de achar o post

--- Trabalho/include/funcoesAuxiares.hpp
+++ Trabalho/include/funcoesAuxiares.hpp
@@ -8,3 +8,19 @@
 
 //---------------------------------------------------------------------------------------
 // Funcoes com template:-----------------------------------------------------------------
+template <typename ch, typename val>
+val &acharPostOuPerfil(const std::map<ch, val> &conjunto, ch idEscolhido)
+{
+    auto procurado = conjunto.find(idEscolhido);
+    if (procurado != conjunto.end())
+        return procurado->second;
+    else
+    {
+        do
+        {
+            std::cout << "Não encontrado. Digite novamente: ";
+            std::cin.ignore();
+            std::cin >> idEscolhido;
+        } while (procurado == conjunto.end()) return procurado->second;
+    }
+}
