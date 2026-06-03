1217fb7b3e51f3aaae0d531fc2a58083036bb55c
Ayla Barcelos <aylabarceloss@gmail.com>
2026-06-02 22:00:02 -0300

    Perfil ligado direto com a Oportunidade sem intermedio de post

--- Trabalho/include/Oportunidades.h
+++ Trabalho/include/Oportunidades.h
@@ -14,6 +14,7 @@ private:
     std::string descricao_; // descrição da oportunidade
     std::string contato_; // contato da oportunidade
     int id_;                // identificador único da oportunidade
+     Perfil perfilAssociado_; // perfil do usuário que publicou a oportunidade
 
 public:
     // Construtor responsável por incializar uma oportunidade
@@ -23,6 +24,7 @@ public:
     int getId() const;
     std::string getDescricao() const;
     std::string getContato() const;
+    Perfil getPerfil() const;
 
     // Edita os dados da oportunidade
     // Pré-condição: só pode ser feita a edição se o usuario for o dono
