69c22e2b2c66188e9f65bd1fa5acbf2860eb79c4
Nick <portesvalina@gmail.com>
2026-06-09 00:52:34 -0300

    Atualização classe Eventos

--- Trabalho/include/Evento.h
+++ Trabalho/include/Evento.h
@@ -10,24 +10,20 @@ class Evento : public Post {
         private:
 
             std::string textoEvento_; //Evento a ser inserido
-            std::string contato_; //Informações de contato
-            Perfil organizador_; //Perfil do organizador do evento
+            std::string contato_; //Informações de contato do organizador
             Perfil perfilAssociadoEvento_;
 
         public:
             Evento(); //Construtor 
-            Evento(const std::string& textoEvento, const std::string& contato,
-                                    const Perfil& organizador, const Perfil& usuario);
+            Evento(const std::string& textoEvento, const std::string& contato, const Perfil& usuario);
 
             //Getters
             std::string getTextoEvento() const;
             std::string getContato() const;
-            Perfil getOrganizador() const;
             Perfil getPerfil() const;
 
             //Metodos
             void editarContato(const std::string& novoContato);
-            void editarOrganizador(const Perfil& novoOrganizador);
             void editarEvento(const std::string& novoTexto);
             void apagarEvento();
 };
