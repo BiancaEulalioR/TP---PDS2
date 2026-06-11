6b987a32fef0720311616e4761552914fb58fabd
Samara Aquino <samarasantosferreiradeaquino@gmail.com>
2026-06-11 18:32:06 -0300

    comentarios doxygen tratamento erros

--- Trabalho/include/Tratamentoerros.hpp
+++ Trabalho/include/Tratamentoerros.hpp
@@ -7,7 +7,9 @@
 /**
  * @brief Verifica se o tipo da entrada digitada 
  * esta coerente com o tipo utilizado
+ * 
  * @tparam T tipo  da entrada
+ * 
  * @param valor Valor a ser verificado
  */
 template <typename T>
@@ -22,36 +24,52 @@ void lerValor(T& valor)
 } 
 
 /**
- * @brief Verifica se o texto digitado 
- * nao esta vazio
+ * @brief Verifica o texto digitado 
+ * 
+ * @throws std::invalid_argument se o texto esta vazio
+ * 
  * @param texto Texto que precisa ser verificado
  */
 void verificaTexto(std::string &texto);
 
 /**
  * @brief Verifica se o email digitado 
- * nao esta vazio e se possui "@" e ".";
+ * 
+ * @throws std::invalid_argument se o email esta vazio
+ * @throws std::invalid_argument se nao tiver "." ou "@"
+ * 
  * @param email Email que precisa ser verificado
  */
 void verificaEmail(std::string &email);
 
 /**
- * @brief Verifica se o email digitado 
- * nao esta vazio e se possui no minimo 6 caracteres
+ * @brief Verificao senha digitado
+ * 
+ * @throws std::invalid_argument se a senha esta vazia 
+ * @throws std::invalid_argument se a senha nao tem no minimo 6 caracteres
+ * 
  * @param senha Senha que precisa ser verificada
  */
 void verificaSenha(std::string &senha);
 
 /**
- * @brief Verifica se o telefone digitado 
- * nao esta vazio e se possui no minimo 11 caracteres numericos
- * @param telefone Telefone que precisa ser verificada
+ * @brief Verifica o telefone digitado 
+ * 
+ * @throws std::invalid_argument se o telefone esta vazio
+ * @throws std::invalid_argument se o telefone nao tem 11 numeros
+ * 
+ * @param telefone Telefone que precisa ser verificado
  */
 void verificaTelefone(std::string &telefone);
 
 /**
  * @brief Verifica se o usuario digitado 
- * nao esta vazio e se nao contem espacos
- * @param usuario Usuario que precisa ser verificada
+ * 
+ * @throws std::invalid_argument se o usuario esta vazio
+ * @throws std::invalid_argument se o usuario contem espacos
+ * @throws std::invalid_argument se o usuario contem algo alem de
+ * letras, numeros, ponto e underline
+ * 
+ * @param usuario Usuario que precisa ser verificado
  */
 void verificaUsuario(std::string &usuario);
