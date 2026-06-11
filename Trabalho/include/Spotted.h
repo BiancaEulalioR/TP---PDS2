54858f647e9f402e9b1b35a20a8e0138ec725e9a
Samara Aquino <samarasantosferreiradeaquino@gmail.com>
2026-06-11 13:51:17 -0300

    Comentários para doxygen

--- Trabalho/include/Spotted.h
+++ Trabalho/include/Spotted.h
@@ -5,7 +5,13 @@
 #include "Perfil.h"
 #include "Post.h"
 
-
+/**
+ * @brief Faz posts anônimos
+ * Armazena o texto da publicação anônima e
+ * o usuário associado a postagem,
+ * faz operações básicas de rede sociais
+ * 
+ */
     class Spotted : public Post {
     private:
     //dados
@@ -13,12 +19,22 @@
     Perfil perfilAssociadoSpotted_;
 
     public:
-
+    /**
+    * @brief Construtor padrão. Inicializa textoSpotted_ e perfilAssociadoSpotted_ vazios
+    */
     Spotted(); 
+
+    /**
+    * @brief Construtor padrão. 
+    * @param textoSpotted_ texto a ser publicado
+    * @param perfilAssociadoSpotted_ usuario que fará a publicação
+    */
     Spotted(std::string spotted_, Perfil& usuario);
-    std::string getSpotted();
-    void apagarPost();  
     Perfil getPerfil() const;
+    std::string getSpotted();
+
+    void apagarPost();  
+    
     };
     
     #endif
