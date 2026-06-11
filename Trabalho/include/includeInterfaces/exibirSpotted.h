e96e6928f76d347a83ad4af411e2ffb35ed39c20
Nick <portesvalina@gmail.com>
2026-06-11 11:05:07 -0300

    Incluindo GerenciadorPerfis

--- Trabalho/include/includeInterfaces/exibirSpotted.h
+++ Trabalho/include/includeInterfaces/exibirSpotted.h
@@ -4,6 +4,7 @@
 #include <map>
 #include "Spotted.h"
 #include "Perfil.h"
+#include "GerenciadorPerfis.h"
 
 void exibirSpotted(std::map<int, Spotted> &postDeSpotted_, Perfil &usuarioLogado, GerenciadorPerfis &gerenciador_);
 
