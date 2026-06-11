6de6cee313fa831b89727de76e1bcb6fb22c441a
BibsHubbie <biancaeulalioramos@gmail.com>
2026-06-11 20:25:09 -0300

    fazendo comentarios doxygen para configuracoes.h e exibirAbaBuscar.h

--- Trabalho/include/includeInterfaces/configuracoes.h
+++ Trabalho/include/includeInterfaces/configuracoes.h
@@ -4,6 +4,18 @@
 #include "Perfil.h"
 #include "GerenciadorPerfis.h"
 
-void configuracoes(Perfil& perfil, GerenciadorPerfis& gerenciador_, int &acesso);
+/**
+ * @brief Exibe a aba de configuracoes, e suas acoes.
+ * Permite visualizar e alterar os dados da conta logada, alem de poder desconectar dela.
+ *
+ * @param perfil referencia que aponta para o perfil logado.
+ * @param acesso int que indica que ha uma conta logada ou nao. Seu valor e zero se ninguem estiver logado.
+ * @param gerenciador_  Armazena o perfil associado
+ *
+ * @details A aba de configuracoes tem as seguintes opcoes em switch case:
+ * Exibir na tela as informacoes do perfil logado, editar essas informacoes do perfil,
+ * apagar a conta, desconectar do perfil logado, retornar ao menu principal.
+ */
+void configuracoes(Perfil &perfil, GerenciadorPerfis &gerenciador_, int &acesso);
 
-#endif 
+#endif
--- Trabalho/include/includeInterfaces/exibirAbaBuscar.h
+++ Trabalho/include/includeInterfaces/exibirAbaBuscar.h
@@ -7,7 +7,22 @@
 #include "Servico.h"
 #include "GerenciadorPerfis.h"
 
-void exibirAbaBuscar(GerenciadorPerfis& gerenciador_, std::map<int, Evento>& postDeEvento_,
-                    std::map<int, Oportunidades>& postDeOportunidade_, std::map<int, Servico>& postDeServico_);
+/**
+ * @brief Exibe a aba de busca, e suas acao de buscar um usuario.
+ * Alem disso, ao encontrar o usuario buscado, essa aba tambem permite vizualizar os posts especificos dessa pessoa
+ *
+ * @param postDeEvento_ referencia que aponta para o map que armazena os posts de evento.
+ * @param postDeOportunidade_ referencia que aponta para o map que armazena os posts de oportunidade.
+ * @param postDeServico_ referencia que aponta para o map que armazena os posts de servico.
+ * @param gerenciador_  Armazena o perfil associado
+ *
+ * @details A aba de configuracoes tem as seguintes opcoes principais em switch case:
+ * buscar pelo perfil e retornar ao menu principal.
+ * dentro da case buscar por perfil, ha outro switch com abas/cases para acessar as publicacoes feitas pelo
+ * usuario do tipo oportunidade, servico e evento.
+ * As publicacoes de spotted nao sao acessadas pois esse tipo de post foi planejado para ser anonimo.
+ */
+void exibirAbaBuscar(GerenciadorPerfis &gerenciador_, std::map<int, Evento> &postDeEvento_,
+                     std::map<int, Oportunidades> &postDeOportunidade_, std::map<int, Servico> &postDeServico_);
 
-#endif 
+#endif
